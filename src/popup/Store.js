cd2886cb26b0f323c2ef47edaee54d22303be105
Steve Binder <email@stevebinder.com>
2019-03-03 20:20:26 -0500

    Simplify store

--- src/popup/Store.js
+++ src/popup/Store.js
@@ -5,62 +5,71 @@ import { getLocalStorage } from 'src/utils/storage';
 
 export const Context = createContext();
 
-export const Provider = props => {
+export const Provider = ({ children }) => {
   const [state, setState] = useState({
     filters: [],
     notifications: [],
     token: '',
   });
-  const actions = {
-    launchAuth: async () => {
-      let token = '';
-      try {
-        token = await launchGithubAuthFlow();
-      } catch (e) {}
-      if (!token) {
-        return;
-      }
-      chrome.storage.local.set({ token });
-      github.setToken(token);
-      const notifications = await github.getNotifications();
-      setState({ ...state, notifications, token });
-    },
-    setFilter: (type, value) => {
-      const isExactFilter = filter => filter.type === type && filter.value === value;
-      const containsExactFilter = state.filters.some(isExactFilter);
-      if (containsExactFilter) {
-        setState({
-          ...state,
-          filters: state.filters.filter(filter => !isExactFilter(filter)),
-        });
-      } else {
-        setState({
-          ...state,
-          filters: [
-            ...state.filters,
-            { type, value },
-          ],
-        });
-      }
-    },
-    syncStorage: async () => {
-      const { notifications = [], token = '' } = await getLocalStorage();
-      if (token) {
-        github.setToken(token);
-        const newNotifications = await github.getNotifications();
-        setState({
-          ...state,
-          notifications: newNotifications,
-          token,
-        });
-      } else {
-        setState({
-          ...state,
-          notifications,
-          token,
-        });
-      }
-    },
+  const launchAuth = async () => {
+    let token = '';
+    try {
+      token = await launchGithubAuthFlow();
+    } catch (e) {}
+    if (!token) {
+      return;
+    }
+    chrome.storage.local.set({ token });
+    github.setToken(token);
+    const notifications = await github.getNotifications();
+    setState({ ...state, notifications, token });
   };
-  return <Context.Provider {...props} value={{ ...state, ...actions }} />;
+  const setFilter = (type, value) => {
+    const isExactFilter = filter => filter.type === type && filter.value === value;
+    const containsExactFilter = state.filters.some(isExactFilter);
+    if (containsExactFilter) {
+      setState({
+        ...state,
+        filters: state.filters.filter(filter => !isExactFilter(filter)),
+      });
+    } else {
+      setState({
+        ...state,
+        filters: [
+          ...state.filters,
+          { type, value },
+        ],
+      });
+    }
+  };
+  const syncStorage = async () => {
+    const { notifications = [], token = '' } = await getLocalStorage();
+    if (token) {
+      github.setToken(token);
+      const newNotifications = await github.getNotifications();
+      setState({
+        ...state,
+        notifications: newNotifications,
+        token,
+      });
+    } else {
+      setState({
+        ...state,
+        notifications,
+        token,
+      });
+    }
+  };
+  return (
+    <Context.Provider
+      value={{
+        ...state,
+        launchAuth,
+        setFilter,
+        syncStorage,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
 };
